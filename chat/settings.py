50b49f1c2bf506a85f7589872a7fd94c903f4526
Ashik <ashik@ivasystems.com>
2026-02-21 00:26:19 +0530

    settings

--- chat/settings.py
+++ chat/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
     'daphne',
+    'django.contrib.staticfiles',
+    
     'channels',
     'accounts',
     'communication',
