6b918364731626c76f997f5705a658f0bd9e5940
Ashik <ashik@ivasystems.com>
2026-02-20 23:51:19 +0530

    Run migrations on startup

--- chat/settings.py
+++ chat/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -102,9 +105,6 @@ LOGOUT_REDIRECT_URL = 'login'
 #     }
 # }
 
-import os
-import pymysql
-pymysql.install_as_MySQLdb()
 
 DATABASES = {
     "default": {
